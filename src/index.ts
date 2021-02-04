cbbfc0852f0457e908b6153dbc64f9932a38443d
周林阳 Zhou Linyang (Intern) <sz.develop.intern3@mhs.local>
2021-02-04 14:51:39 +0800

    添加全局异常处理

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,17 @@ import todoRouter from './controller/todoController';
 import connect from './utils/dbconnect';
 import bodyParser from 'koa-bodyparser';
 import cors from './utils/cors';
+import { Result } from './entity/Result';
 const app = new Koa();
 connect(); // 建立mongodb连接
+// 添加全局异常处理
+app.use(async (ctx, next) => {
+    try {
+      await next()
+    } catch (error) {
+      ctx.body = new Result<null>(20001, null, 'error');
+    }
+});
 app.use(cors);
 app.use(bodyParser());
 app.use(noteRouter.routes());
